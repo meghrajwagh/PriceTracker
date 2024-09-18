5d83c721bd79112e28dbe12b0129680e73899ef4
meghrajwagh <meghrajwagh2004@gmail.com>
2024-09-18 22:52:36 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from datetime import datetime
+
+
+links: dict[str, str] = {"LOQ_R7_8845HS_4050": "https://amzn.in/d/4NL6AvO",
+                         "KatanaA17_R7_8845HS_4060": "https://amzn.in/d/c4Scwyz"}
+data: dict[str, list] = {"Name": [["Date", "price"]], "LOQ_R7_8845HS_4050": []}
+# temp = []
+
+
+def checkPrice():
+    if data["LOQ_R7_8845HS_4050"][0][0] == datetime.today().strftime('%Y-%m-%d'):
+        driver = webdriver.Chrome()
+        for name in links:
+            if name not in data:
+                data[name] = []
+            driver.get(links[name])
+            price = driver.find_element(By.XPATH,
+                                        "//*[@id=\"corePriceDisplay_desktop_featur"
+                                        "e_div\"]/div[1]/span[3]/span[2]/span[2]")
+            data[name].append([price.text, datetime.today().strftime('%Y-%m-%d')])
+
+
+def printData():
+    for name in data:
+        print(f"{name}: ", end="")
+        for vals in data[name]:
+            print(vals)
+
+
+checkPrice()
+printData()
