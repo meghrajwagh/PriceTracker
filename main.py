049080557a548ed07d3762c42d86c8b3366f8004
meghrajwagh <meghrajwagh2004@gmail.com>
2024-09-20 22:39:44 +0530

    Added support for Flipkart links

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from selenium import webdriver
+from time import sleep
 from selenium.webdriver.common.by import By
 from datetime import datetime
 import csv
-from os import path
 
-
-
+today = datetime.today().strftime('%Y-%m-%d')
 links: list = [
-        ["LOQ_R7_8845HS_4050", "https://amzn.in/d/4NL6AvO"],
-        ["KatanaA17_R7_8845HS_4060", "https://amzn.in/d/c4Scwyz"]
+        ["LOQ_R7_8845HS_4050", "https://amzn.in/d/4NL6AvO", "Amazon"],
+        ["KatanaA17_R7_8845HS_4060", "https://amzn.in/d/c4Scwyz", "Amazon"],
+        ["KatanaA15_R7_8845HS_4050", "https://amzn.in/d/c6A2Jzh", "Amazon"],
+        ["Victus_R7_8845HS_4050", "https://amzn.in/d/46xtM5l", "Amazon"],
+        ["TUF-A15_R7_7735HS_4050", "https://amzn.in/d/c39zkX7", "Amazon"],
+        ["LOQ_R7_8845HS_4050", "https://dl.flipkart.com/s/pxd95DuuuN", "Flipkart"],
+        ["NitroV_R7_7735HS_4050", "https://dl.flipkart.com/s/pE8I3EuuuN", "Flipkart"],
+        ["LOQ_R7_7735HS_4050", "https://dl.flipkart.com/s/pEZhrauuuN", "Flipkart"],
+        ["LegionSlim5_R7_7840HS_4050", "https://dl.flipkart.com/s/pEAFcauuuN", "Flipkart"],
+        ["LOQ_R7_7435HS_4060", "https://dl.flipkart.com/s/pEZZH1uuuN", "Flipkart"]
         ]
-
 data: list  = []
 lastUpdatedDate = None
 
@@ -22,28 +28,32 @@ with open('data.csv', 'r') as dataFile:
 
 
 def checkPrice():
-    # if data["LOQ_R7_8845HS_4050"][0][0]==datetime.today().strftime('%Y-%m-%d'):      
-    # (Fix: store data in CSV file)
-    driver = webdriver.Chrome()
-    for lis in links:
-        name = lis[0]
-        link = lis[1]
-        driver.get(link)
-        priceStr = driver.find_element(By.XPATH,
-                                    "//*[@id=\"corePriceDisplay_desktop_featur"
-                                    "e_div\"]/div[1]/span[3]/span[2]/span[2]").text
-        price = int(priceStr.replace(',',''))
-        data.append([name, price, datetime.today().strftime('%Y-%m-%d')])
+    if lastUpdatedDate != today:
+        driver = webdriver.Chrome()
+        for lis in links:
+            prdtName = lis[0]
+            link = lis[1]
+            site = lis[2]
+            driver.get(link)
+            if site == "Amazon":
+                priceStr = driver.find_element(By.XPATH,
+                                            "//*[@id=\"corePriceDisplay_desktop_featur"
+                                            "e_div\"]/div[1]/span[3]/span[2]/span[2]").text
+                price = int(priceStr.replace(',',''))
+                data.append([prdtName, price, today])
+            else:
+                priceStr = driver.find_element(By.CLASS_NAME,"CxhGGd").text
+                price = int(priceStr.replace('₹','').replace(',',''))
+                data.append([prdtName, price, today])
+    else:
+        print("Data is Up-to-date!")
 
 
 def writeData():
-    if lastUpdatedDate != datetime.today().strftime('%Y-%m-%d'):
         with open('data.csv', 'w', newline='') as dataFile:
             write = csv.writer(dataFile)
             for row in data:
                 write.writerow(row)
-    else:
-        print("Data is Up-to-date!")
 
 
 
@@ -54,7 +64,6 @@ def printData():
         for row in read:
             print(row)
     print("\n\n")
-
 
 checkPrice()
 writeData()
